b43adf23e232c5175a72270fe2aa37a5c6f54a2e
jaysavani <39219880+jaysavani@users.noreply.github.com>
2018-09-22 16:01:33 +0530

    Add files via upload

---
+++ addition of matrix.py
@@ -0,0 +1,28 @@
+def addmatrics():
+
+    X = [[12,7,3],
+        [4 ,5,6],
+        [7 ,8,9]]
+
+    Y = [[5,8,1],
+        [6,7,3],
+        [4,5,9]]
+
+    # X = input('enter the first matrix\n')
+    # Y = input('enter the second matrix\n')
+
+    result = [[0,0,0],
+             [0,0,0],
+             [0,0,0]]
+
+
+    for i in range(len(X)):
+        for j in range (len(X[0])):
+
+            result [i][j] = X[i][j] + Y[i][j]
+
+    for r in result:
+
+        print(r)
+
+addmatrics()
